0a860b0fd37fc8f3674be0b502749d17596baf86
rahul <killbusyness@gmail.com>
2024-08-21 17:17:55 +0530

    small updates

--- src/presentDrinks.js
+++ src/presentDrinks.js
@@ -1,11 +1,13 @@
 import fetchDrinks from "./fetchDrinks.js";
 import displayDrinks from "./displayDrinks.js";
-
+import setDrink from "./setDrink.js";
 const presentDrinks = async (URL) => {
   const data = await fetchDrinks(URL);
 
   const section = await displayDrinks(data);
-  console.log(section);
+  if (section) {
+    setDrink(section);
+  }
 };
 
 export default presentDrinks;
---
+++ src/setDrink.js
@@ -0,0 +1,11 @@
+const setDrink = (section) => {
+  section.addEventListener("click", (e) => {
+    e.preventDefault();
+    const clicked_drink_id = e.target.parentElement.dataset.id;
+    console.log(clicked_drink_id);
+  });
+
+  localStorage.setItem("clicked_drink", clicked_drink_id);
+};
+
+export default setDrink;
